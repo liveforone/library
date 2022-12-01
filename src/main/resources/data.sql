65081926cc07ab6e7fa89318d97aa55203f98078
liveforone <yc4852@gmail.com>
2022-12-01 18:52:44 +0900

    Feat pre data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into book(book_code, borrow_count, count, title) values("var5443s", 0, 5, "어린왕자");
+insert into book(book_code, borrow_count, count, title) values("srx3345t", 0, 4, "자바의 정석");
+insert into book(book_code, borrow_count, count, title) values("sdf2245a", 0, 3, "데이터 베이스");
+insert into book(book_code, borrow_count, count, title) values("fde5467i", 0, 2, "빅데이터");
+insert into book(book_code, borrow_count, count, title) values("gei7743p", 0, 6, "뉴욕");
